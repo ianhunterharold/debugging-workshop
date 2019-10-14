143ea3e52de63441e0dcedca5b148b81cc1dfb30
ianhunterharold <ianhunterharold@gmail.com>
2019-10-14 16:25:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
   main();
 });
 
 const URL = "https://icanhazdadjoke.com/";
 
 function main(){
-  fetchJoke()
+  attachEventListener();
 };
 
 
@@ -19,7 +18,9 @@ function main(){
       },
     })
     .then(response => response.json()) // this line turns our response into json format 
-    .then(json => postJoke(json)); // have our function work on the returned json formatted data 
+    .then(json => {
+      console.log("joke", json)
+      appendJoke(json)}); // have our function work on the returned json formatted data 
     // .then(jokeData => joke = jokeData.joke) unsure about the format of this then statement. 
   }
 
@@ -29,22 +30,21 @@ function main(){
   const username = document.getElementById('name-input');
   let joke;
 
-function postJoke(individualJoke){
- 
+function appendJoke(individualJoke){
+  let jokeText = `${username.value} says: ${individualJoke.joke}`;
+  let li = document.createElement('li');
+  li.innerText = jokeText;
+  jokeList.append(li);
+}
+
+function  attachEventListener() {
+  
   form.addEventListener('submit', (event) => {
     event.preventDefault()
-
     if(username !== ""){
       fetchJoke() 
     }
-
-    newJokeLi.textContent = `${username.value} says: ${individualJoke.joke}`
-    const oldList = document.createElement('li');
-    oldList.append(newJokeLi);
-    jokeList.append(oldList);
-    jokeList.append(newJokeLi);
-
-
   });
-
 }
+
+// moral of the story, have your even listener in a seperate function, to seperate your concerns so that when you start your page, you do not create new event listeners every time.
