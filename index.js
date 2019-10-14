a2c33ff1c20f5d9c14086143267e9b88ef834ff0
ianhunterharold <ianhunterharold@gmail.com>
2019-10-14 15:33:48 -0700

    Done.

--- index.js
+++ index.js
@@ -10,42 +10,41 @@ function main(){
 };
 
 
-//get is not needed in method because if you dont say what it is, it implies that it's a GET/ didnt need Content-type only needed for POST/PATCH
+
+//get is not needed in method because if you dont say what it is, it implies that it's a GET/ didnt need Content-type only needed for POST/PATCH bc api rules stated so clearly. 
   function fetchJoke(){
     fetch(URL, {
       headers: {
         "Accept": "application/json"
-      }
+      },
     })
     .then(response => response.json()) // this line turns our response into json format 
     .then(json => postJoke(json)); // have our function work on the returned json formatted data 
     // .then(jokeData => joke = jokeData.joke) unsure about the format of this then statement. 
   }
 
-
-
-function postJoke(individualJoke){
-
   const form = document.getElementById('joke-form');
   const jokeList = document.getElementById('joke-list');
   const newJokeLi = document.createElement('li');
-  const username = document.getElementById('name-input').value;
+  const username = document.getElementById('name-input');
   let joke;
 
-
+function postJoke(individualJoke){
+ 
   form.addEventListener('submit', (event) => {
     event.preventDefault()
-    // console.log('submit',form)
 
-    if(username === "") return;
-    fetchJoke()
-  
-    newJokeLi.textContent = `<span class="username">${username} says:</span> ${individualJoke}`
-    console.log(`<span class="username">${username} says:</span> ${individualJoke}`)
-    jokeList.appendChild(newJokeLi);
-    form.appendChild(jokeList);
+    if(username !== ""){
+      fetchJoke() 
+    }
+
+    newJokeLi.textContent = `${username.value} says: ${individualJoke.joke}`
+    const oldList = document.createElement('li');
+    oldList.append(newJokeLi);
+    jokeList.append(oldList);
+    jokeList.append(newJokeLi);
 
 
-  })
+  });
 
 }
