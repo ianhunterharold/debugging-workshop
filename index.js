dbd1de3606b2a3fceca934481c674832ebd534fe
ianhunterharold <ianhunterharold@gmail.com>
2019-10-14 13:07:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  
+  main();
+});
 
+const URL = "https://icanhazdadjoke.com/";
+
+function main(){
+  fetchJoke()
+};
+
+
+//get is not needed in method because if you dont say what it is, it implies that it's a GET/ didnt need Content-type only needed for POST/PATCH
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    fetch(URL, {
       headers: {
         "Accept": "application/json"
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(response => response.json()) // this line turns our response into json format 
+    .then(json => postJoke(json)); // have our function work on the returned json formatted data 
+    // .then(jokeData => joke = jokeData.joke) unsure about the format of this then statement. 
   }
 
+
+
+function postJoke(individualJoke){
+
+  const form = document.getElementById('joke-form');
+  const jokeList = document.getElementById('joke-list');
+  const newJokeLi = document.createElement('li');
+  const username = document.getElementById('name-input').value;
+  let joke;
+
+
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // console.log('submit',form)
 
     if(username === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+  
+    newJokeLi.textContent = `<span class="username">${username} says:</span> ${individualJoke}`
+    console.log(`<span class="username">${username} says:</span> ${individualJoke}`)
+    jokeList.appendChild(newJokeLi);
+    form.appendChild(jokeList);
+
+
   })
-})
+
+}
